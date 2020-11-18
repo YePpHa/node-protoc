6c23372153da124b51af1b7159be84baec09c488
achingbrain <alex@achingbrain.net>
2020-11-18 14:20:29 +0000

    chore: print stdout/stderr after command exits

--- protoc.sh
+++ protoc.sh
@@ -2,9 +2,17 @@
 
 const { protoc } = require('./')
 
-protoc(process.argv.slice(2), {}, (err) => {
+protoc(process.argv.slice(2), {}, (err, stdout, stderr) => {
   if (err) {
     console.error(err)
     process.exit(1)
+  }
+
+  if (stdout) {
+    console.info(stdout)
+  }
+
+  if (stderr) {
+    console.error(stderr)
   }
 })
