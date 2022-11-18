0304e1745e202c25def82b6d53127b29a9ee3d1a
Bruno Ely <brunobely@gmail.com>
2022-11-18 00:52:48 +0000

    Add support for Apple M1 chips.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -15,7 +15,8 @@ const releases = {
 };
 
 const platform = process.platform;
-const arch = process.arch === "x64" ? "x86_64" : "x86_32";
+const arch =
+  process.arch === "x64" || process.arch === "arm64" ? "x86_64" : "x86_32";
 const release = platform + "_" + arch;
 const protocDirectory = path.join(__dirname, "..", "protoc");
 
