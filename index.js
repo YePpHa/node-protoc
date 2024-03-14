94cdcf913b03e4995578866cc63d507845ee7904
J.I. /podhead <117015142+ji-soft@users.noreply.github.com>
2024-03-14 20:06:39 +0100

    added Typescript

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const fs = require("fs");
 const { exec } = require('child_process');
 const path = require('path');
 const cp = require("child_process");
-// In einer Datei, z.B. currentDir.js
+const { type } = require("os");
 
-function mycallback(null1,in1,err){
-    console.log(err)
+
+
+function createProtobuff(language,outputPath,proto_file,proto_path){
+    let directoryPath,fileName
+    if(proto_path==undefined) {
+    directoryPath = path.dirname(proto_file);
+    fileName = path.basename(proto_file);
+    console.log(`Directory: ${directoryPath}`);
+    console.log(`File Name: ${fileName}`);
+    }
+    else{
+        directoryPath=proto_path
+        fileName=proto_file
+    }
+    new ProtobuffGenerator().generateProtobuf(language,directoryPath,fileName,outputPath)
+}
+    
+function mycallback(){
+    //console.log("created ")
   }
 class ProtobuffGenerator {
     /**
@@ -26,89 +43,97 @@ class ProtobuffGenerator {
      * @returns {Promise} A promise that resolves with the output of the command execution or rejects with an error.
      * @throws {Error} If the specified language is not supported.
      */
-    generateProtobuf(language, proto_path,proto_file, outputPath) {
-        let command;
-        switch (language) {
-            case 'go':
-                // Example for Go --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative
-                command = `npx protoc --go_out=${outputPath} --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'java':
-                // Example for Java
-                command = `npx protoc --java_out=${outputPath} --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'python':
-                // Example for Python
-                command = `npx protoc --python_out=${outputPath} --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'csharp':
-                // Example for C#
-                command = `npx protoc --objc_out=${outputPath} --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'ruby':
-                // Example for Ruby
-                command = `npx protoc --ruby_out=${outputPath} --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'objc':
-                // Example for Objective-C
-                command = `npx protoc --objc_out=${outputPath} --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'php':
-                // Example for PHP
-                command = `npx protoc --php_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'dart':
-                // Example for Dart
-                command = `npx protoc --dart_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'rust':
-                // Example for Rust
-                command = `npx protoc --rust_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'swift':
-                // Example for Swift
-                command = `npx protoc --swift_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'kotlin':
-                // Example for Kotlin
-                command = `npx protoc --kotlin_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'scala':
-                // Example for Scala
-                command = `npx protoc --scala_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
-                break;
-            case 'js':
-                // Example for JavaScript/TypeScript
-                const protocGenTsPath = `${process.cwd()}/frontend/node_modules/ts-protoc-gen/bin/protoc-gen-ts`; // todo
-                const outDir = `${process.cwd()}/frontend/src`;
-                const protoPath = `${process.cwd()}/protos/`;
-          //      command = `
-          //  npx protoc --plugin="protoc-gen-ts=${protocGenTsPath}" --ts_out="service=grpc-node,mode=grpc-js:${outDir}" --js_out="import_style=commonjs,binary:${outDir}" --proto_path="${protoPath}" ${proto_path}
-          //  npx protoc --plugin="protoc-gen-ts=${protocGenTsPath}" --ts_out="service=grpc-web:${outDir}" --js_out="import_style=commonjs,binary:${outDir}" --proto_path="${protoPath}" ${proto_path}
-          //  `;
-            command = `npx protoc --js_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
+  
+     
+        generateProtobuf(language, proto_path,proto_file, outputPath, additionalCommands) {
+            let command;
+            let arr
+            switch (language) {
+                case 'go':
+                    // Example for Go --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative
+                    command = `npx protoc --go_out=${outputPath} ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'java':
+                    // Example for Java
+                    command = `npx protoc --java_out=${outputPath}  ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'python':
+                    // Example for Python
+                    command = `npx protoc --python_out=${outputPath}  ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'csharp':
+                    // Example for C#
+                    command = `npx protoc --objc_out=${outputPath}  ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'ruby':
+                    // Example for Ruby
+                    command = `npx protoc --ruby_out=${outputPath}  ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'objc':
+                    // Example for Objective-C
+                    command = `npx protoc --objc_out=${outputPath}  ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'php':
+                    // Example for PHP
+                    command = `npx protoc --php_out=${outputPath}   ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'dart':
+                    // Example for Dart
+                    command = `npx protoc --dart_out=${outputPath}   ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'rust':
+                    // Example for Rust
+                    command = `npx protoc --rust_out=${outputPath}   ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'swift':
+                    // Example for Swift
+                    command = `npx protoc --swift_out=${outputPath}   ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'kotlin':
+                    // Example for Kotlin
+                    command = `npx protoc --kotlin_out=${outputPath}   ${additionalCommands?additionalCommands:""} --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'scala':
+                    // Example for Scala
+                    command = `npx protoc --scala_out=${outputPath}  ${additionalCommands?additionalCommands:""}  --proto_path=${proto_path} ${proto_file}`;
+                    break;
+                case 'js':
+                command = `npx protoc --js_out=${outputPath}  --proto_path=${proto_path} ${proto_file}`;
+                    break;
+               case 'ts':
+                        // Example for JavaScript/TypeScript
+                    command= `npx protoc --ts_out=${outputPath} --proto_path=${proto_path} ${proto_file}`
+                    break;
+                    default:
+                        
+                        if(Array.isArray(language)){
+                            arr = true;
+                            for (let arg of language){
+                                this.generateProtobuf(...arg);
+                            }
+                        } else if(typeof(language) == 'object'){
+                            arr = true;
+                            for (let arg in language){
+                                this.generateProtobuf(...arg);
 
-                break;
-            default:
-                throw new Error(`Unsupported language: ${language}`);
-        }
-       console.log(command)
-        // fs.mkdirSync(outputPath, { recursive: true });
-       exec(command, function(err, stdout, stderr) {
-        console.log(stdout);
+                            }
+                        } else {
+                            throw new Error(`Unsupported language: ${language}`);
+                        }
+            }
+       if(arr!=true){
+       } 
+if(arr!=true)
+        {       exec(command, function(err, stdout, stderr) {
         if (err) {
-            mycallback(err);
             console.error(err);
             return;
         }
-        console.log(stderr);
-        mycallback(null, stdout, stderr);
-    })
-      
-
-     
+        console.log("__________\n"+"finished command: " + JSON.stringify([language, proto_path,proto_file, outputPath, additionalCommands]))
+//        mycallback(null, stdout, stderr);
+    }
+    )
+    }
     }
  }
-
-
-module.exports ={ProtobuffGenerator}
+module.exports ={createProtobuff,ProtobuffGenerator,cli}
